d534b6b1d36f82692dcd9a758ee663b92beacf15
AleksanderBak <aleksanderbak9@gmail.com>
2024-10-15 23:06:31 +0200

    Experimenting with async printing response from llm

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import asyncio
+
 import streamlit as st
+
+from agent import HomeAgent
 
 st.set_page_config(
     page_title="HomeMind",
@@ -8,21 +12,43 @@ st.set_page_config(
 )
 st.title("HomeMind")
 
-st.sidebar.title("Settings", anchor="bottom")
+st.markdown(
+    """
+    <style>
+        .st-emotion-cache-janbn0 {
+            flex-direction: row-reverse;
+            text-align: right;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
+agent = HomeAgent()
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "assistant", "content": "How can I help you?", "avatar": "🏠"}
     ]
 
+
 for msg in st.session_state.messages:
     st.chat_message(msg["role"], avatar=msg.get("avatar")).write(msg["content"])
 
+
+async def print_async_response(prompt):
+    async for response in agent.get_response(prompt):
+        st.session_state.messages[-1]["content"] += response
+        st.chat_message(
+            "assistant",
+            avatar="🏠",
+        ).write(response)
+
+
 if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
     st.session_state.messages.append(
-        {"role": "assistant", "content": msg["content"], "avatar": "🏠"}
+        {"role": "user", "content": prompt, "avatar": "🧑‍💻"}
     )
-    st.chat_message("assistant", avatar="🏠").write(msg["content"])
+    st.chat_message("user", avatar="🧑‍💻").write(prompt)
+
+    asyncio.run(print_async_response(prompt))
